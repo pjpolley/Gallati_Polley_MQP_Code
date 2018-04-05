cbcf241891a3359c35ecbd71f1923f2f88ddc3ff
pjpolley <pjpolley@wpi.edu>
2018-04-05 00:59:34 -0400

    PID control added (NO I or D because they're far more challenging to implement with unity)

--- New Unity Project/Assets/HandyHands/scripts/Joints/B3Behavior.cs
+++ New Unity Project/Assets/HandyHands/scripts/Joints/B3Behavior.cs
@@ -3,14 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class B3Behavior : MonoBehaviour {
+    float lastError = 0.0f;
+    float errorAccumulation = 0.0f;
+    float lastDesiredPosition = float.MaxValue;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.localEulerAngles = new Vector3(0, 0, Globals.B3Pos);
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get current position and desired position
+        float desiredPosition = Globals.B3DesiredPosition;
+        float currentPosition = transform.localEulerAngles.z;
+        //calculate error
+        float error = desiredPosition - currentPosition;
+        //make sure to reset error acumulation if target changes suddenly
+        if (Mathf.Abs((lastDesiredPosition - desiredPosition)) > Globals.errorAccumulationClear)
+        {
+            errorAccumulation = 0;
+            lastDesiredPosition = desiredPosition;
+        }
+        //make sure we're moving optimally
+        while (error > 180 || error < -180)
+        {
+            if (error > 180)
+            {
+                error = (error - 180) * -1;
+            }
+            else
+            {
+                error = (error + 180) * -1;
+            }
+        }
+        //add to error accumulation for PID
+        errorAccumulation += error;
+        //calculate PID
+        float changeInPosition = Globals.PID_Calculation(error, lastError, errorAccumulation);
+        //make needed change
+        transform.localEulerAngles = new Vector3(0, 0, currentPosition + changeInPosition);
+        //update variables for next loop
+        lastError = error;
+        Globals.B3ActualPosition = transform.localEulerAngles.z;
     }
 }
--- New Unity Project/Assets/HandyHands/scripts/RotateHandParts.cs
+++ New Unity Project/Assets/HandyHands/scripts/RotateHandParts.cs
@@ -18,12 +18,15 @@ public class RotateHandParts : MonoBehaviour
     void Start()
     {
         //set things up
+
         using (StreamWriter sw = new StreamWriter(unityReadyToGo))
         {
             sw.WriteLine("G");
         }
         //wait for confirmation of ready to go
         while (!File.Exists(WFAReadyToGo)) { }
+        switchToWFA();
+
     }
 
     // Update is called once per frame
@@ -33,52 +36,79 @@ public class RotateHandParts : MonoBehaviour
         {
             // Read and show each line from the file.
             string line = "";
+            float data = 0;
             using (StreamReader sr = new StreamReader(filePath))
             {
                 while ((line = sr.ReadLine()) != null)
                 {
                     switch (line.Substring(0, 2)){
                         case "T1":
-                            Globals.T1Pos = (float)System.Convert.ToDouble(line.Substring(2));
+                            data = (float)System.Convert.ToDouble(line.Substring(2));
+                            Globals.T1DesiredPosition = optimizeInputAngles(data);
                             break;
                         case "T2":
-                            Globals.T2Pos = (float)System.Convert.ToDouble(line.Substring(2));
+                            data = (float)System.Convert.ToDouble(line.Substring(2));
+
+                            Globals.T2DesiredPosition = optimizeInputAngles(data);
                             break;
                         case "A1":
-                            Globals.A1Pos = (float)System.Convert.ToDouble(line.Substring(2));
+                            data = (float)System.Convert.ToDouble(line.Substring(2));
+
+                            Globals.A1DesiredPosition = optimizeInputAngles(data);
                             break;
                         case "A2":
-                            Globals.A2Pos = (float)System.Convert.ToDouble(line.Substring(2));
+                            data = (float)System.Convert.ToDouble(line.Substring(2));
+
+                            Globals.A2DesiredPosition = optimizeInputAngles(data);
                             break;
                         case "A3":
-                            Globals.A3Pos = (float)System.Convert.ToDouble(line.Substring(2));
+                            data = (float)System.Convert.ToDouble(line.Substring(2));
+
+                            Globals.A3DesiredPosition = optimizeInputAngles(data);
                             break;
                         case "B1":
-                            Globals.B1Pos = (float)System.Convert.ToDouble(line.Substring(2));
+                            data = (float)System.Convert.ToDouble(line.Substring(2));
+
+                            Globals.B1DesiredPosition = optimizeInputAngles(data);
                             break;
                         case "B2":
-                            Globals.B2Pos = (float)System.Convert.ToDouble(line.Substring(2));
+                            data = (float)System.Convert.ToDouble(line.Substring(2));
+
+                            Globals.B2DesiredPosition = optimizeInputAngles(data);
                             break;
                         case "B3":
-                            Globals.B3Pos = (float)System.Convert.ToDouble(line.Substring(2));
+                            data = (float)System.Convert.ToDouble(line.Substring(2));
+
+                            Globals.B3DesiredPosition = optimizeInputAngles(data);
                             break;
                         case "C1":
-                            Globals.C1Pos = (float)System.Convert.ToDouble(line.Substring(2));
+                            data = (float)System.Convert.ToDouble(line.Substring(2));
+
+                            Globals.C1DesiredPosition = optimizeInputAngles(data);
                             break;
                         case "C2":
-                            Globals.C2Pos = (float)System.Convert.ToDouble(line.Substring(2));
+                            data = (float)System.Convert.ToDouble(line.Substring(2));
+
+                            Globals.C2DesiredPosition = optimizeInputAngles(data);
                             break;
                         case "C3":
-                            Globals.C3Pos = (float)System.Convert.ToDouble(line.Substring(2));
+                            data = (float)System.Convert.ToDouble(line.Substring(2));
+
+                            Globals.C3DesiredPosition = optimizeInputAngles(data);
                             break;
                         case "D1":
-                            Globals.D1Pos = (float)System.Convert.ToDouble(line.Substring(2));
+                            data = (float)System.Convert.ToDouble(line.Substring(2));
+
+                            Globals.D1DesiredPosition = optimizeInputAngles(data);
                             break;
                         case "D2":
-                            Globals.D2Pos = (float)System.Convert.ToDouble(line.Substring(2));
+                            data = (float)System.Convert.ToDouble(line.Substring(2));
+
+                            Globals.D2DesiredPosition = optimizeInputAngles(data);
                             break;
                         case "D3":
-                            Globals.D3Pos = (float)System.Convert.ToDouble(line.Substring(2));
+                            data = (float)System.Convert.ToDouble(line.Substring(2));
+                            Globals.D3DesiredPosition = optimizeInputAngles(data);
                             break;
                     }
 
@@ -92,8 +122,42 @@ public class RotateHandParts : MonoBehaviour
         }
     }
 
+    private float optimizeInputAngles(float input)
+    {
+        float data = input;
+        while (data > 360)
+        {
+            data -= 360;
+        }
+        while (data < 0)
+        {
+            data += 360;
+        }
+        return data;
+    }
+
     private void switchToWFA()
     {
+        
+        using (StreamWriter sw = new StreamWriter(filePath))
+        {
+            sw.WriteLine("T1" + Globals.T1ActualPosition);
+            sw.WriteLine("T2" + Globals.T2ActualPosition);
+            sw.WriteLine("A1" + Globals.A1ActualPosition);
+            sw.WriteLine("A2" + Globals.A2ActualPosition);
+            sw.WriteLine("A3" + Globals.A3ActualPosition);
+            sw.WriteLine("B1" + Globals.B1ActualPosition);
+            sw.WriteLine("B2" + Globals.B2ActualPosition);
+            sw.WriteLine("B3" + Globals.B3ActualPosition);
+            sw.WriteLine("C1" + Globals.C1ActualPosition);
+            sw.WriteLine("C2" + Globals.C2ActualPosition);
+            sw.WriteLine("C3" + Globals.C3ActualPosition);
+            //sw.WriteLine("D1" + Globals.D1ActualPosition);
+            sw.WriteLine("D2" + Globals.D2ActualPosition);
+            sw.WriteLine("D3" + Globals.D3ActualPosition);
+            sw.WriteLine("From unity");
+        }
+        
         File.Delete(mutexUnityTurn);
         using (StreamWriter sw = new StreamWriter(mutexFileTurn))
         {
