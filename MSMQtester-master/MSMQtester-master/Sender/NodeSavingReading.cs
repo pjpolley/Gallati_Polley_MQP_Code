dc23078bb5758a15d4726dd0689c63008f30844b
pjpolley <pjpolley@wpi.edu>
2018-04-12 20:19:07 -0400

    Added node saving and reading functionality

---
+++ MSMQtester-master/MSMQtester-master/Sender/NodeSavingReading.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    public class NodeSavingReading
+    {
+
+        public void pushDataToFile(String fileLocation, List<Node> nodesToSave)
+        {
+            List<Node> listOfNodes = nodesToSave.ToList<Node>();
+            string output = JsonConvert.SerializeObject(listOfNodes);
+            using (StreamWriter sw = new StreamWriter(Globals.TreeSaveLocation))
+            {
+                sw.WriteLine(output);
+            }
+        }
+
+        public List<Node> GetDataFromFile(String fileLocation)
+        {
+            if (File.Exists(fileLocation))
+            {
+                string inputData = File.ReadAllText(Globals.TreeSaveLocation);
+                try
+                {
+                    return JsonConvert.DeserializeObject<List<Node>>(inputData);
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
